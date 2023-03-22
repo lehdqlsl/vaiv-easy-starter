a432fe5a3e96b38ca69bc89e96148e9b5c5b9b5f
lehdqlsl <lehdqlsl@naver.com>
2023-03-22 22:47:45 +0900

    init

---
+++ vaiv-easy-starter/src/main/java/com/vaiv/sample/VaivEasyStarterApplication.java
@@ -0,0 +1,13 @@
+package com.vaiv.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaivEasyStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaivEasyStarterApplication.class, args);
+	}
+
+}
---
+++ vaiv-easy-starter/src/test/java/com/vaiv/sample/VaivEasyStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaiv.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaivEasyStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
